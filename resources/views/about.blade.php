b13f9c0a8dec0f22d2c099004ea5a0d699008dc6
jacekk <github.com@jacek-k.net>
2019-03-03 12:35:49 +0100

    added some pages with basic layout;

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'About us')
+
+@section('content')
+    <h1>Working on it</h1>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Contact')
+
+@section('content')
+    <h1>Contact form will be here someday :)</h1>
+@endsection
